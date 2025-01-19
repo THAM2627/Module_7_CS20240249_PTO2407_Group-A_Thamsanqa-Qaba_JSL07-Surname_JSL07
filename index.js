89aef0b970d2c72bd1d65820f41bcd577b95015d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-01-19 03:14:32 +0200

    updating modal

--- index.js
+++ index.js
@@ -56,8 +56,8 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   //  🚨 Close the modal when the close button is clicked
-  function closeModal() {
-    document.getElementById("modal").style.display = "none";
+  closeModal.addEventListener('click', function () {
+    modal.style.display = 'none';
 
-  };
+  });
 });
